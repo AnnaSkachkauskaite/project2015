79c488e22b5d47d31ed55db68a9d2896d70218f0
AnnaSkachkauskaite <skachkauskaite.anna@gmail.com>
2015-08-21 17:34:38 +0300

    upload

---
+++ try/try/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _try.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> fileUpload)
+        {
+            foreach (var file in fileUpload)
+            {
+                if (file == null) continue;
+                string path = AppDomain.CurrentDomain.BaseDirectory + "UploadedFiles/";
+                string filename = System.IO.Path.GetFileName(file.FileName);
+                file.SaveAs(Server.MapPath("~/Files/" + filename));
+            }
+
+            return RedirectToAction("Finish");
+        }
+
+        public ActionResult Finish()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ try/try/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_try.MvcApplication" Language="C#" %>
